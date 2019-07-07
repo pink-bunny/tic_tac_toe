79bfed761029d5ba1d848e9f65a61d8a8da5b9cc
Svetlana Razheva <rajevasvetlana@gmail.com>
2019-07-07 16:25:17 +0300

    Tic Tac Toe page.

--- app/containers/App/App.js
+++ app/containers/App/App.js
@@ -10,11 +10,9 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import { Switch, Route } from 'react-router-dom';
 
-import HomePage from 'containers/HomePage/Loadable';
-import FeaturePage from 'containers/FeaturePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
+import TicTacToe from 'containers/TicTacToe/Loadable';
 import Header from 'components/Header';
-import Footer from 'components/Footer';
 import './style.scss';
 
 const App = () => (
@@ -23,15 +21,14 @@ const App = () => (
       titleTemplate="%s - React.js Boilerplate"
       defaultTitle="React.js Boilerplate"
     >
-      <meta name="description" content="A React.js Boilerplate application" />
+      <meta name="description" content="Tic tac toe" />
     </Helmet>
-    <Header />
+
     <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/features" component={FeaturePage} />
+      <Route exact path="/" component={TicTacToe} />
       <Route path="" component={NotFoundPage} />
     </Switch>
-    <Footer />
+
   </div>
 );
 
