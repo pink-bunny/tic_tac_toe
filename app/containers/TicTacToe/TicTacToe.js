6c4d1e54b5e72ba7f5cbb9e1123503ea20d69148
Svetlana Razheva <rajevasvetlana@gmail.com>
2019-07-16 21:29:24 +0300

    Winner logic.

--- app/containers/TicTacToe/TicTacToe.js
+++ app/containers/TicTacToe/TicTacToe.js
@@ -8,7 +8,7 @@ export default class TicTacToe extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentPlayerMove: 'player_x',
+      currentPlayer: 'player_x',
       player_x: 'x',
       player_o: 'o',
       items: []
@@ -17,30 +17,81 @@ export default class TicTacToe extends React.Component {
   handleClick(i){
     // Update items array
     let updatedItemsArr = this.state.items;
-    let playerSign = this.state.currentPlayerMove
-    updatedItemsArr[i] = this.state[playerSign];
-
-    // Update current player move
-    let currentPlayerMove = this.state.currentPlayerMove;
+    let currentPlayerName = this.state.currentPlayer
+    updatedItemsArr[i] = {
+      ...this.state.items[i],
+      value: this.state[currentPlayerName]
+    };
 
     this.setState({
       ...this.state,
-      currentPlayerMove: (currentPlayerMove === 'player_x' ? 'player_o' : 'player_x'),
+      currentPlayer: (currentPlayerName === 'player_x' ? 'player_o' : 'player_x'),
       items: updatedItemsArr
     })
+    return this.checkWinner(currentPlayerName);
   };
+
+  checkWinner(currentPlayerName) {
+    let checkedSign = this.state[currentPlayerName];
+    let stringLength = Math.sqrt(this.props.fieldLength);
+    let arr = this.state.items;
+    let multiArr = [];
+    let horizontalArr = [];
+    let verticalArr = [];
+    let diagonalTLRB = [];
+    let diagonalLBRT = [];
+
+    // Multidimentional array
+    let multiStart = 0;
+    let multiEnd = multiStart + stringLength;
+    for (let i=0; i < stringLength; i++) {
+      multiArr.push(arr.slice(multiStart, multiEnd));
+      multiStart = multiEnd;
+      multiEnd = multiStart + stringLength;
+    }
+
+    // Horizontal
+    horizontalArr = multiArr.slice();
+
+    // Vertical
+    for (let el=0; el < stringLength; el++) {
+      let subArr = [];
+      for (let r=0; r < stringLength; r++) {
+        subArr.push(multiArr[r][el]);
+      }
+      verticalArr.push(subArr);
+    }
+
+    // Diagonal top-left/right-bottom
+    for(let i=0; i < stringLength; i++) {
+      diagonalTLRB.push(multiArr[i][i]);
+    }
+
+    // Diagonal left-bottom/top-right
+    let el = stringLength - 1;
+    for(let i=0; i < stringLength; i++) {
+      diagonalLBRT.push(multiArr[i][el]);
+      el--
+    }
+
+    console.log('checkedSign', checkedSign);
+  }
 
   componentWillMount(){
     let length = this.props.fieldLength;
     let arr = [];
     for (let i=0; i<length; i++) {
-      arr.push(null);
+      arr.push({
+        key: i,
+        value: null
+      });
     }
     this.setState({ items: arr })
   }
 
   render() {
-    let items = this.state.items
+    let items = this.state.items;
+    let playerSign = this.state.currentPlayer;
 
     return (
       <article className="ttt">
@@ -48,6 +99,7 @@ export default class TicTacToe extends React.Component {
           <title>Tic Tac Toe</title>
         </Helmet>
 
+        <h2 className="ttt-title">It is <span className="ttt-title__sign">{this.state[playerSign]}</span> turn.</h2>
         <div className="ttt-nav">
           <Link className="ttt-nav__btn" to="/">
              Step Back
@@ -56,16 +108,15 @@ export default class TicTacToe extends React.Component {
             Step Forward
           </Link>
         </div>
-        <p>It is {this.state.currentPlayerMove} turn </p>
 
         <div className="ttt-field">
           {items.map((item, index) =>
             <div
-              key={index}
+              key={item.key}
               onClick={this.handleClick.bind(this, index)}
               className="ttt-field__item"
             >
-              {item}
+              {item.value}
             </div>
           )}
         </div>
@@ -83,3 +134,4 @@ export default class TicTacToe extends React.Component {
 TicTacToe.propTypes = {
   fieldLength: PropTypes.number
 };
+//
