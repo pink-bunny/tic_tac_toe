30935a845f488596ebdb25ae75261d5ec55a4d1c
Svetlana Razheva <rajevasvetlana@gmail.com>
2019-08-10 13:43:57 +0300

    Reducer\ tests.

---
+++ app/containers/TicTacToe/tests/reducer.test.js
@@ -0,0 +1,50 @@
+import ticTacToeReducer from '../reducer';
+import {
+  increaseTotalSets,
+  increasePlayerWin
+} from '../actions';
+
+describe('TicTacToe Reducer', () => {
+  let state;
+  beforeEach(() => {
+    state = {
+      fieldLength: 9,
+      winner: null,
+      currentPlayer: 'player1',
+      player1: {
+        value: 'x',
+        wins: 0
+      },
+      player2: {
+        value: 'o',
+        wins: 0
+      },
+      setsPlayed: 0
+    };
+  });
+
+  it('should return the initial state', () => {
+    const expectedResult = state;
+    expect(ticTacToeReducer(undefined, {})).toEqual(expectedResult);
+  });
+
+  it('should handle INCREASE_TOTAL_SETS', () => {
+    const expectedResult = {
+      ...state,
+      setsPlayed: state.setsPlayed + 1
+    };
+    expect(ticTacToeReducer(state, increaseTotalSets())).toEqual(expectedResult);
+  });
+
+  it('should handle INCREASE_PLAYER_WIN', () => {
+    const winner = 'player1';
+    const expectedResult = {
+      ...state,
+      [winner]: {
+        ...state[winner],
+        wins: state[winner].wins + 1
+      }
+    };
+    expect(ticTacToeReducer(state, increasePlayerWin(winner))).toEqual(expectedResult);
+  });
+});
