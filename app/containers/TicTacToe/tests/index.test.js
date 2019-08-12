5a6d00a0833975b1f5a860cd4cee8b315c746cbf
Svetlana Razheva <rajevasvetlana@gmail.com>
2019-08-12 11:22:26 +0300

    TicTacToe tests. Snapshots.

---
+++ app/containers/TicTacToe/tests/index.test.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
+
+import Nav from 'components/TicTacToe/Nav';
+import InfoTitle from 'components/TicTacToe/InfoTitle';
+import ResultsPanel from 'components/TicTacToe/ResultsPanel';
+import FieldCell from 'components/TicTacToe/FieldCell';
+import TicTacToe from '../TicTacToe';
+
+describe.only('<TicTacToe />', () => {
+  let props;
+  beforeEach(() => {
+    props = {
+      fieldLength: 9,
+      winner: null,
+      currentPlayer: 'player1',
+      player1: {
+        value: 'x',
+        wins: 0
+      },
+      player2: {
+        value: 'o',
+        wins: 0
+      },
+      setsPlayed: 0,
+    };
+  });
+  describe('<Nav />', () => {
+    it('component exists inside tic tac toe', () => {
+      const renderedComponent = shallow(<TicTacToe {...props} />);
+      expect(renderedComponent.find(Nav).length).toBe(1);
+    });
+
+    it('component has orrect HTML', () => {
+      const tree = renderer
+        .create(<Nav onBack={() => {}} onForward={() => {}} />)
+        .toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+  });
+
+  it('should render a <InfoTitle />', () => {
+    const renderedComponent = shallow(<TicTacToe {...props} />);
+    expect(renderedComponent.find(InfoTitle).length).toBe(1);
+  });
+
+  it('should render a <ResultsPanel />', () => {
+    const renderedComponent = shallow(<TicTacToe {...props} />);
+    expect(renderedComponent.find(ResultsPanel).length).toBe(1);
+  });
+
+  it('should render a <FieldCell />', () => {
+    const renderedComponent = shallow(<TicTacToe {...props} />);
+    expect(renderedComponent.find(FieldCell).length).toBe(9);
+  });
+});
